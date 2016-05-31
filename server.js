d4611d0ce5092ec754511a145f5e570544ed2866
boriska70@gmail.com <boriska70@gmail.com>
2016-05-31 17:17:54 +0300

    clean urls working on refresh too

--- server.js
+++ server.js
@@ -18,12 +18,16 @@ app.set('port', (process.env.PORT || 4000));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// send all requests to index.html so browserHistory works
+
 app.use(function (req, res, next) {
-    //Set permissive CORS header - looks like a security breach when used with asterix
+    //Does not work - going to index.html to ensure that clean urls working on refresh
+/*    //Set permissive CORS header - looks like a security breach when used with asterix
     res.set('Access-Control-Allow-Origin','*');
     //Disable caching
-    res.set('Cache-Control', 'no-cache');
-    next();
+    res.set('Cache-Control', 'no-cache');*/
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 app.listen(app.get('port'), function () {
