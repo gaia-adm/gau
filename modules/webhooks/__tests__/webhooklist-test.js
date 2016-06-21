255ffe4cd3dab03904487cb37453e7d3af811eff
boriska70@gmail.com <boriska70@gmail.com>
2016-06-21 09:59:47 +0300

    cleanup

--- modules/webhooks/__tests__/webhooklist-test.js
+++ modules/webhooks/__tests__/webhooklist-test.js
@@ -8,9 +8,8 @@ import WebhookList from '../WebhookList';
 //NOTE: cannot be executed in NODE_ENV=production environment (see http://stackoverflow.com/questions/29586928/react-minified-exception-occurred)
 
 describe('WebhookList', ()=> {
-  var staticData='[{"datasource":"dockerhub","eventType":"push","tsField":"push_data.pushed_at","createdAt":1463989059120,"apiToken":"1111-2222-3333-4444","tenantId":1056454051,"token":"69e9428cf222222051a8a528d686c97dd5aad5d1","hookUrl":"https://webhook.server.com/wh/1111-2222-3333-4444/69e9428cf222222051a8a528d686c97dd5aad5d1"},{"datasource":"github","eventType":"push","apiToken":"1111-2222-3333-4444","tenantId":1056454051,"token":"37b9d70f87fcf988888829d2888358ef5bbb5394","hookUrl":"https://webhook.server.com/wh/1111-2222-3333-4444/37b9d70f87fcf988888829d2888358ef5bbb5394","tsField":"repository.pushed_at"}]';
-  var data = JSON.parse(staticData);
-  var whl = TestUtils.renderIntoDocument( <WebhookList data={data}/>);
+  var staticData=[{'datasource':'dockerhub','eventType':'push','tsField':'push_data.pushed_at','createdAt':1463989059120,'apiToken':'1111-2222-3333-4444','tenantId':1056454051,'token':'69e9428cf222222051a8a528d686c97dd5aad5d1','hookUrl':'https://webhook.server.com/wh/1111-2222-3333-4444/69e9428cf222222051a8a528d686c97dd5aad5d1'},{'datasource':'github','eventType':'push','apiToken':'1111-2222-3333-4444','tenantId':1056454051,'token':'37b9d70f87fcf988888829d2888358ef5bbb5394','hookUrl':'https://webhook.server.com/wh/1111-2222-3333-4444/37b9d70f87fcf988888829d2888358ef5bbb5394','tsField':'repository.pushed_at'}];
+  var whl = TestUtils.renderIntoDocument( <WebhookList data={staticData}/>);
 
   const constWhlNode = ReactDOM.findDOMNode(whl);
 
