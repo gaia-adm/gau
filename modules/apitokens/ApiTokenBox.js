8ab68139fb21c15a573938ff870c3d642e1ce1c0
boriska70@gmail.com <boriska70@gmail.com>
2016-06-30 17:59:10 +0300

    Get the API Token from the system you logged in

--- modules/apitokens/ApiTokenBox.js
+++ modules/apitokens/ApiTokenBox.js
@@ -6,14 +6,18 @@ var ApiTokenBox = React.createClass({
 
   getInitialState(){
     console.log('Initialize token box');
-    var myToken = sessionStorage.getItem('gaia.at');
-    if (!myToken){
+    var myTokenValue = sessionStorage.getItem('gaia.at.value');
+    var myTokenBirthday = sessionStorage.getItem('gaia.at.birthday');
+    if (!myTokenValue || !myTokenBirthday) {
       return {
-        token: 'none'
+        token: null,
+        createdAt: null
       }
     } else {
       return {
-        token: myToken
+        token: myTokenValue,
+        createdAt: myTokenBirthday,
+        errorMessage: null
       }
     }
   },
@@ -26,17 +30,37 @@ var ApiTokenBox = React.createClass({
       cache: false,
       success: function (data) {
         console.log('Body: ' + JSON.stringify(data));
-        sessionStorage.setItem('gaia.at', JSON.stringify(data));
-        this.setState({token: data, errorMessage: ''});
+        sessionStorage.setItem('gaia.at.value', data.access_token);
+        sessionStorage.setItem('gaia.at.birthday', new Date(Number(data.createdAt)));
+        this.setState({
+          token: sessionStorage.getItem('gaia.at.value'),
+          createdAt: sessionStorage.getItem('gaia.at.birthday'),
+          errorMessage: ''
+        });
       }.bind(this),
       error: function (xhr, status, err) {
         console.error(xhr.url, status, err.toString());
+        sessionStorage.removeItem('gaia.at.value');
+        sessionStorage.removeItem('gaia.at.birthday');
         this.setState({
-          token: 'error',
+          token: sessionStorage.getItem('gaia.at.value'),
+          createdAt: sessionStorage.getItem('gaia.at.birthday'),
           errorMessage: err.toString() + ' (Reason: ' + (xhr.responseJSON ? xhr.responseJSON.message : 'unkonwn') + ')'
         });
       }.bind(this)
     });
+  },
+
+  createMarkup() {
+    var infoString;
+    if (this.state.token && this.state.createdAt) {
+      infoString = "<p><b>API token: </b>" + this.state.token + "</p><p><b>Created at: </b>" + this.state.createdAt + "</p>"
+    } else if (this.state.errorMessage) {
+      infoString = "<p>API token is not available, the last error happened: "+this.state.errorMessage+"</p><p>Please press <b>'Get My Token'</b> button below in order to obtain the token</p>";
+    } else {
+      infoString = "<p>API token is not available, please press <b>'Get My Token'</b> button below in order to obtain the token</p>";
+    }
+    return {__html: infoString};
   },
 
   render: function () {
@@ -44,14 +68,15 @@ var ApiTokenBox = React.createClass({
       <div>
         <h2 style={{'textAlign': 'center', 'backgroundColor': '#e7e7e7', 'color': '#003366'}}>API Token</h2>
         <div className='tokenBox'>
-          <p>TOKEN: {this.state.token}</p>
-          <Button bsStyle='info' bsSize='large' onClick={this.getMyToken()}>GetMyToken</Button>
+          <div dangerouslySetInnerHTML={this.createMarkup()}/>
+          <Button bsStyle='info' bsSize='large' onClick={this.getMyToken}>GetMyToken</Button>
           <p/>
         </div>
       </div>
     )
   }
-  
+
+
 });
 
 module.exports = ApiTokenBox;
