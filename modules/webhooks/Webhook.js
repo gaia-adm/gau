c76d94f319cd019ca265d066269ff2cbfde6910f
boriska70 <boriska70@gmail.com>
2016-07-10 23:09:45 +0300

    fix problems found by lint

--- modules/webhooks/Webhook.js
+++ modules/webhooks/Webhook.js
@@ -9,7 +9,8 @@ var Webhook = React.createClass({
     event: React.PropTypes.string,
     hookUrl: React.PropTypes.string,
     tsField: React.PropTypes.string,
-    tid: React.PropTypes.string
+    tid: React.PropTypes.string,
+    createdAt: React.PropTypes.object
   },
 
   getInitialState: function () {
@@ -30,20 +31,13 @@ var Webhook = React.createClass({
     if (isNaN(date.getTime())) {
       return ''
     }
-    ;
     var hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
     var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
     var seconds = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
     var year = date.getFullYear();
     var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth()+1);
-    var date = (date.getDate() < 10 ? '0' : '') + date.getDate();
-    return hours + ':' + minutes + ':' + seconds + ' ' + date + '/' + month + '/' + year;
-  },
-  getPart: function (date, part) {
-    var result;
-    if (part === 'hours') {
-      result = date.getHours()
-    }
+    var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+    return hours + ':' + minutes + ':' + seconds + ' ' + day + '/' + month + '/' + year;
   },
   render: function () {
     return (
